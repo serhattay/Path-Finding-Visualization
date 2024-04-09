f14f7449f340ec8885e5349f6d9b0b5dd5229a20
ozanmart <ozanmart5480@gmail.com>
2024-04-10 01:23:01 +0300

    random grid generator

--- src/Graph.java
+++ src/Graph.java
@@ -1,18 +1,15 @@
+import java.util.PriorityQueue;
+import java.util.Random;
+
 public class Graph {
     protected static Tile[][] grid = new Tile[Map.ROW][Map.COL];
     protected static Tile characterLocation;
 
+    protected static Random random = new Random();
+
+
     public static Tile aStarAlgorithm(Tile destinationTile) {
 
-        Tile current = characterLocation;
-
-        while (current != destinationTile) {
-
-            for (Adjacent adjacent: characterLocation.getAdjacencies()) {
-
-            }
-
-        }
         return null;
     }
 
@@ -55,8 +52,33 @@ public class Graph {
         }
         return grid;
     }
-    public static Tile[][] generateRandomGrid() {
-        return null;
+    public static Tile[][] generateRandomGrid(boolean isSourceRandom) {
+        // source is not random in this case
+        if (!isSourceRandom) {
+            gridHelper(7, 0);
+        } else {
+            int sourceRow = random.nextInt(Map.ROW);
+            int sourceCol = random.nextInt(Map.COL);
+            grid[sourceRow][sourceCol] = new Tile(sourceRow, sourceCol, false, true);
+            gridHelper(sourceRow, sourceCol);
+        }
+        return grid;
+    }
+    private static void gridHelper(int sourceRow, int sourceCol) {
+        for (int i = 0; i < Map.ROW; i++) {
+            for (int j = 0; j < Map.COL; j++) {
+                int rand = random.nextInt(5);
+                if (i == sourceRow && j == sourceCol) {
+                    grid[i][j] = new Tile(i, j, false, true);
+                    continue;
+                }
+                if (rand == 0) {
+                    grid[i][j] = new Tile(i, j, true);
+                } else {
+                    grid[i][j] = new Tile(i, j, false);
+                }
+            }
+        }
     }
 
     public static Tile getCharacterLocation() {
